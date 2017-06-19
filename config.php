6c11d8ea1a19f41d696d4efe37b5f28a21aa28a9
lightningexperience <lightningexperience@users.noreply.github.com>
2017-06-18 23:39:51 -0400

    Add files via upload

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
-$conn = mysql_connect("localhost", "database-user","database-pass");
-$db = mysql_select_db("database-name"); 
-$Webmaster_Email ="webmaster@youremail.com";
-$Webmaster_Name ="Webmaster Name";
-$Website_Name ="Website Name";
+$db = parse_url($_ENV[“DATABASE_URL”]);
+define(“dbname”, trim($db[“path”],”/”));
+define(“user”, $db[“user”]);
+define(“password”, $db[“pass”]);
+define(“host”, $db[“host”]);
+$conn = pg_connect("host=$hostname dbname=$database user=$username password=$password"); 
+$Webmaster_Email =“ffaizi@salesforce.com”;
+$Webmaster_Name =“from heroku mortgage cal“;
+$Website_Name ="heroku mortgage”;
 ?>
